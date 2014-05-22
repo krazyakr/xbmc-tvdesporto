ff669c8e8478ceac02f56a3738162bae3c344da9
krazyakr <rmtorres@gmail.com>
2014-05-22 13:06:34 +0100

    Moved to source dir

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 rm build/*
 mkdir tmp
 mkdir tmp/plugin.video.tvdesporto
-cp -r * tmp/plugin.video.tvdesporto/
-rm -r tmp/plugin.video.tvdesporto/build
-rm tmp/plugin.video.tvdesporto/*.sh
+cp -r source/* tmp/plugin.video.tvdesporto/
+#rm -r tmp/plugin.video.tvdesporto/build
+#rm tmp/plugin.video.tvdesporto/*.sh
 rm tmp/plugin.video.tvdesporto/*~
-rm -r tmp/plugin.video.tvdesporto/tmp
+#rm -r tmp/plugin.video.tvdesporto/tmp
 cd tmp
 zip -r ../build/plugin.video.tvdesporto.zip plugin.video.tvdesporto
 cd ..
--- debug.sh
+++ debug.sh
@@ -1,15 +1,15 @@
 # ~/.bashrc
 
-mkdir tmp
-mkdir tmp/plugin.video.tvdesporto
-cp -r * tmp/plugin.video.tvdesporto/
-rm -r tmp/plugin.video.tvdesporto/build
-rm tmp/plugin.video.tvdesporto/*.sh
-rm tmp/plugin.video.tvdesporto/*~
-rm -r tmp/plugin.video.tvdesporto/tmp
-cd tmp
-cp -r plugin.video.tvdesporto ~/.xbmc/addons/
-cd ..
-rm -r tmp
+#mkdir tmp
+#mkdir tmp/plugin.video.tvdesporto
+#cp -r * tmp/plugin.video.tvdesporto/
+#rm -r tmp/plugin.video.tvdesporto/build
+#rm tmp/plugin.video.tvdesporto/*.sh
+#rm tmp/plugin.video.tvdesporto/*~
+#rm -r tmp/plugin.video.tvdesporto/tmp
+#cd tmp
+cp -r source/* ~/.xbmc/addons/plugin.video.tvdesporto/
+#cd ..
+#rm -r tmp
 
 
